50c344a304ce2119006017d73de215254d12383f
José Ramón Hernández Muñoz <joseramonhernandezmunoz20@gmail.com>
2022-02-11 17:51:51 -0600

    updates

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-import axios from 'axios';
+
+
+
+var getData = function (){
+    var name = document.getElementById("name").value;
+    console.log(name);
+}
 
 const apiKey = '25745ce0da2954b896c845ab208d6851';
 
 const getInitialData = async () =>{
-    const respuesta = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}&units=metric`);
+    const respuesta = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Huamantla&appid=${apiKey}&units=metric`);
 }
 
-const container = document.getElementById('div1');
-
-const boton = document.querySelector('button');
-
-const accion = () => {
-    const imagen = document.createElement('img');
-}
-
-container.appendChild(imagen);
-
-boton.addEventListener('click', accion);
